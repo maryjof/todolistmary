197c2c5f77d2d23bd9ddf14fad8341d221ea81d5
marielyn52 <marielyn.otero@gmail.com>
2019-12-13 14:58:24 +0000

    first commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,14 +2,50 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			array: []
+		};
+	}
+	render() {
+		return (
+			<div className="text-center mt-5">
+				<h1> To Do List </h1>
+				<div className="parent text-center mt-5">
+					<input
+						className="col-6"
+						onKeyDown={e => {
+							if (e.key === "Enter") {
+								this.setState({
+									array: this.state.array.concat(e.target.value)
+								});
+								e.target.value = "";
+							}
+						}}
+					/>
+					<ul className="list-group">
+						{this.state.array.map((item, index) => {
+							return (
+								<li
+									key={index}
+									className=" col-6 mx-auto list-group-item d-flex justify-content-between">
+									{item}
+									<i
+										className="fas fa-trash-alt"
+										onClick={() => {
+											let x = this.state.array;
+											x.splice(index, 1);
+											this.setState({ array: x });
+										}}
+									/>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
